65d5b8e31567f716027007ccc47cdc91f37f85df
Tanishq-Prajapati <prajapatitanishq57@gmail.com>
2024-11-09 17:30:44 +0530

    prefinal version

--- cynestia/settings.py
+++ cynestia/settings.py
@@ -148,7 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 STATIC_ROOT = '/home/tanishq/Documents/cynestia-web/static/'
 
 # Default primary key field type
